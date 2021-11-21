c4d688352f619ce663af5e8f1c3e5f831381cdec
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-21 09:59:18 +0100

    piechart and doughnutchart completed

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,53 @@
-import React from 'react';
+// import React from 'react';
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+// const ExampleChart = () => {
+//   return <div>chart</div>;
+// };
+
+// export default ExampleChart;
+
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      
+      chart: {
+        
+        caption: "Stars Per Languages",
+      
+        theme: "candy",
+        decimals: 0,
+        pieRadius: "45%",
+        showPercentValues: 0,
+        
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Doughnut2d;
+export default ChartComponent;
