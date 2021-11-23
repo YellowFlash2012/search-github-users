f43e0a106644b261b145986e6d8a47c66f02fcf4
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-23 09:13:49 +0100

    added access restriction to the dashboard

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
       domain={process.env.REACT_APP_AUTH_DOMAIN}
       clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
       redirectUri={window.location.origin}
+      cacheLocation='localstorage'
     >
       <GithubProvider>
         <App />
